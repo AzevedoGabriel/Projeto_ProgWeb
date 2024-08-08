e9776d5dfb168d8463c230126764bea75d4e223e
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2024-08-08 20:52:53 -0300

    conexão com bd e primeira tabela

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,6 @@ const port = 3000;
 app.use(express.json())
 app.use(alunoRoutes)
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!")
-})
-
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`)
 })
